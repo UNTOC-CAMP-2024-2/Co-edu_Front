2207d77a1653ac8a37064fb10194cfed1c371c3a
jimini1026 <128363259+jimini1026@users.noreply.github.com>
2024-11-19 19:00:59 +0900

    과제 컴포넌트 로고 상태에 따라 변경

--- src/pages/mentor&menteePage/menteePage/menteeMainPage/MenteeMainPage.js
+++ src/pages/mentor&menteePage/menteePage/menteeMainPage/MenteeMainPage.js
@@ -3,25 +3,30 @@ import CommonMainComponent from "../../components/CommonMainTitle";
 import CommonComponent from "../../components/CommonComponent";
 import Assignment from "../../components/MainPageAssignment";
 
+// 멘티 페이지에서의 Assignment컴포넌트의 type
+// 전체 과제 -> done / undone
+// 내가 제출한 과제 -> done
+// 패드백 모아보기 -> gotFeedback
+
 const MenteeMainPage = () => {
   return (
     <div className="mx-14 mb-10">
       <CommonMainComponent />
       <div className="flex gap-5 mt-3">
         <CommonComponent componentTitle={"전체 과제"}>
-          <Assignment />
-          <Assignment />
-          <Assignment />
+          <Assignment type={"done"} />
+          <Assignment type={"undone"} />
+          <Assignment type={"done"} />
         </CommonComponent>
         <CommonComponent componentTitle={"내가 제출한 과제"}>
-          <Assignment />
-          <Assignment />
-          <Assignment />
+          <Assignment type={"done"} />
+          <Assignment type={"done"} />
+          <Assignment type={"done"} />
         </CommonComponent>
         <CommonComponent componentTitle={"과제 피드백하기"}>
-          <Assignment />
-          <Assignment />
-          <Assignment />
+          <Assignment type={"gotFeedback"} />
+          <Assignment type={"gotFeedback"} />
+          <Assignment type={"gotFeedback"} />
         </CommonComponent>
       </div>
     </div>
--- src/pages/mentor&menteePage/mentorPage/mentorMainPage/MentorMainPage.js
+++ src/pages/mentor&menteePage/mentorPage/mentorMainPage/MentorMainPage.js
@@ -3,25 +3,25 @@ import CommonMainComponent from "../../components/CommonMainTitle";
 import Assignment from "../../components/MainPageAssignment";
 import CommonComponent from "../../components/CommonComponent";
 
+// 멘토 페이지에서의 Assignment컴포넌트의 type
+// 전체 과제 -> done / undone / halfDone (멘티들이 다 했는지에 따라)
+// 패드백 모아보기 -> gaveFeedbackAll / notGaveFeedbackAll (모든 멘티에게 피드백을 주었는지에 따라)
+
 const MentorMainPage = () => {
   return (
     <div className="mx-14 mb-10">
       <CommonMainComponent />
       <div className="flex gap-5 mt-3">
         <CommonComponent componentTitle={"전체 과제"}>
-          <Assignment />
-          <Assignment />
-          <Assignment />
+          <Assignment type={"done"} />
+          <Assignment type={"undone"} />
+          <Assignment type={"halfDone"} />
         </CommonComponent>
-        <CommonComponent componentTitle={"과제 생성"}>
-          <Assignment />
-          <Assignment />
-          <Assignment />
-        </CommonComponent>
+        <CommonComponent componentTitle={"과제 생성"} />
         <CommonComponent componentTitle={"과제 피드백하기"}>
-          <Assignment />
-          <Assignment />
-          <Assignment />
+          <Assignment type={"gaveFeedbackAll"} />
+          <Assignment type={"notGaveFeedbackAll"} />
+          <Assignment type={"gaveFeedbackAll"} />
         </CommonComponent>
       </div>
     </div>
