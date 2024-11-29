b991ab9d255891b4d0eb851ca47500b40a18fa9a
jimini1026 <128363259+jimini1026@users.noreply.github.com>
2024-11-29 17:29:22 +0900

    경로 에러 해결

--- src/pages/mentor&menteePage/mentorPage/mentorFeedbackPage/MentorFeedbackPage.js
+++ src/pages/mentor&menteePage/mentorPage/mentorFeedbackPage/MentorFeedbackPage.js
@@ -1,15 +1,15 @@
 import React from "react";
-import FeedbackAssignment from "./components/FeedbackAssignment";
+import FeedbackPageAssignment from "../../components/FeedbackPageAssignment";
 
 const MentorFeedbackPage = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-5 mb-10">
       <div className="text-lightBlack text-3xl p-14">피드백 모아보기</div>
       <div className="flex flex-col gap-5">
-        <FeedbackAssignment type={"gaveFeedbackAll"} />
-        <FeedbackAssignment type={"gaveFeedbackFew"} />
-        <FeedbackAssignment type={"notGaveFeedbackAll"} />
-        <FeedbackAssignment type={"gaveFeedbackAll"} />
+        <FeedbackPageAssignment type={"gaveFeedbackAll"} />
+        <FeedbackPageAssignment type={"gaveFeedbackFew"} />
+        <FeedbackPageAssignment type={"notGaveFeedbackAll"} />
+        <FeedbackPageAssignment type={"gaveFeedbackAll"} />
       </div>
     </div>
   );
