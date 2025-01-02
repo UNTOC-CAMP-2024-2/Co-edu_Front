cf33c793f176c76db6b68dba35d6cb927f1f39d0
hyojeong130 <hyojeong0109@pusan.ac.kr>
2025-01-02 15:30:38 +0900

    과제세부내용및실행
    코드 작성 및 실행결과 제외

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -15,6 +15,7 @@ import MentorMakingAssignmentPage from "../pages/mentor&menteePage/mentorPage/me
 import MenteeReadingAssignmentPage from "../pages/mentor&menteePage/menteePage/meteeReadingAssignmentPage/MenteeReadingAssignmentPage";
 import MentorEditAssignmentPage from "../pages/mentor&menteePage/mentorPage/mentorEditAssignmentPage/MentorEditAssignmentPage";
 import MentorReadingAssignmentsPage from "../pages/mentor&menteePage/mentorPage/mentorReadingAssignmentsPage/MentorReadingAssignmentsPage";
+import MenteeDetailAssignmentPage from "../pages/mentor&menteePage/menteePage/menteeDetailAssignmentPage/MenteeDetailAssignmentPage";
 
 const AppRouter = () => {
   const router = createBrowserRouter([
@@ -88,6 +89,10 @@ const AppRouter = () => {
         {
           path: "read",
           element: <MenteeReadingAssignmentPage />,
+        },
+        {
+          path: "detail",
+          element: <MenteeDetailAssignmentPage />,
         },
       ],
     },
