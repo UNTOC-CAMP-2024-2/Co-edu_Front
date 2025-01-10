edd661791d93fe1ce5ae8697b01425fbad1652d1
jimini1026 <128363259+jimini1026@users.noreply.github.com>
2025-01-10 16:53:09 +0900

    개별 과제 페이지 api 연결 완료

--- src/api/mentee.js
+++ src/api/mentee.js
@@ -8,3 +8,12 @@ export const getAssignmentList = async ({ classCode }) => {
   });
   return response.data;
 };
+
+export const getAssignmentDetail = async ({ assignmentId }) => {
+  const response = await axiosInstance.get("/assign/info", {
+    params: {
+      assignment_id: assignmentId,
+    },
+  });
+  return response.data;
+};
