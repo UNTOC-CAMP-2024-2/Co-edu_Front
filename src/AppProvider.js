5f03dcec7d5c694dcfed18aaa3b7156e52558966
minecoby <minecoby@naver.com>
2025-06-21 01:18:40 +0900

    임시적 토큰을 저장하도록

--- src/AppProvider.js
+++ src/AppProvider.js
@@ -1,12 +1,35 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 export const Context = createContext();
 
 const AppProvider = ({ children }) => {
-  const [token, setToken] = useState(null);
-  const [username, setUsername] = useState(null);
+  const [token, setTokenState] = useState(() => localStorage.getItem("token"));
+  const [username, setUsernameState] = useState(() => localStorage.getItem("username"));
   const [classCode, setClassCode] = useState(null);
   const [className, setClassName] = useState(null);
+
+  const setToken = (value) => {
+    setTokenState(value);
+    if (value) {
+      localStorage.setItem("token", value);
+    } else {
+      localStorage.removeItem("token");
+    }
+  };
+
+  const setUsername = (value) => {
+    setUsernameState(value);
+    if (value) {
+      localStorage.setItem("username", value);
+    } else {
+      localStorage.removeItem("username");
+    }
+  };
+
+  useEffect(() => {
+    setTokenState(localStorage.getItem("token"));
+    setUsernameState(localStorage.getItem("username"));
+  }, []);
 
   return (
     <Context.Provider
